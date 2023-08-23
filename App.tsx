9fece153fc841560aeb43d37a41dc2d0d46830b7
Júlia Malta <juliamalta12@gmail.com>
2023-08-23 18:51:36 -0300

    token

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+import { StyleSheet, View } from 'react-native';
+import Notification from './src/Components/Token';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Notification/>
     </View>
   );
 }
@@ -13,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
+    paddingTop:80,
   },
 });
